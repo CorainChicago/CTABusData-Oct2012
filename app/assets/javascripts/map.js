861c3f92acbdcd6b9535bbd2de856e753a166878
CorainChicago <chaysmagan@gmail.com>
2016-06-15 17:08:38 -0500

    add button to show bus routes, data WIP

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,62 +1,65 @@
+var citymap;
+var map;
+
 $.ajax({
-    type: "GET",
-    contentType: "application/json; charset=utf-8",
-    url: 'stops/data_stop_location_and_bus_count',
-    dataType: 'json',
-    data: "{}", 
-    async: true,
-    success: function (data) {
-      initMap(data);
-    },
-    error: function (result) {
-      console.log("error");
-       error();
-    }
-    });
-  
-   // var citymap = {
-   //  chicago: {
-   //    center: {lat: 41.878, lng: -87.629},
-   //    population: 2714856
-   //  },
-   //  newyork: {
-   //    center: {lat: 40.714, lng: -74.005},
-   //    population: 8405837
-   //  },
-   //  losangeles: {
-   //    center: {lat: 34.052, lng: -118.243},
-   //    population: 3857799
-   //  },
-   //  vancouver: {
-   //    center: {lat: 49.25, lng: -123.1},
-   //    population: 603502
-   //  }
-   //  }; 
-  function initMap(data) {
-    // Create the map.
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 11,
-      center: {lat: 41.8781, lng: -87.6298},
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-    });
-
-    var citymap = data
+  type: "GET",
+  contentType: "application/json; charset=utf-8",
+  url: 'stops/data_stop_location_and_bus_count',
+  dataType: 'json',
+  data: "{}", 
+  async: true,
+  success: function (data) {
+    citymap = data;
+    console.log(data);
     console.log(citymap)
-    // Construct the circle for each value in citymap.
-    // Note: We scale the area of the circle based on the population.
-    for (var city in data) {
-      // Add the circle for this city to the map.
-      var cityCircle = new google.maps.Circle({
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        map: map,
-        center: citymap[city].center,
-        radius: Math.sqrt(citymap[city].buses) * 100
-      });
-    }
-   }
+  },
+  error: function (result) {
+    console.log("error");
+     error();
+  }
+  }); 
 
-   
+function initMap() {
+  // Create the map.
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {lat: 41.8781, lng: -87.6298},
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
+
+  // Construct the circle for each value in citymap.
+  // Note: We scale the area of the circle based on the population.
+  // for (var city in data) {
+  //   // Add the circle for this city to the map.
+  //   var cityCircle = new google.maps.Circle({
+  //     strokeColor: '#FF0000',
+  //     strokeOpacity: 0.8,
+  //     strokeWeight: 2,
+  //     fillColor: '#FF0000',
+  //     fillOpacity: 0.35,
+  //     map: map,
+  //     center: citymap[city].center,
+  //     radius: Math.sqrt(citymap[city].buses) * 50
+  //   });
+  }
+  function showBusStops(citymap, map){
+    for (var city in citymap) {
+      console.log(citymap[city]);
+    // Add the circle for this city to the map.
+    var cityCircle = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: citymap[city].center,
+      radius: Math.sqrt(citymap[city].buses) * 50
+    });
+  }
+
+
+
+$(document).on("click", "#show_bus_stops", function(citymap, map){
+   showBusStops(citymap, map);
+});
