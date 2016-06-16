263b447c05eb8f2d00c1a2d81e6df3f9c34d6a98
CorainChicago <chaysmagan@gmail.com>
2016-06-16 02:07:08 -0500

    fixing issue in index page with display of data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
     bus_hash = BusRoute.bus_hash_by_stop_count
-    p stop_hash = Stop.stop_hash_by_bus_route_count
+    stop_array = Stop.stop_array_by_bus_route_count
     @longest = BusRoute.order_by_stops(bus_hash).first
-    p @most_active = Stop.order_by_bus_route_count(stop_hash)
+    @most_active = Stop.order_by_bus_route_count(stop_array)
   end
 end
