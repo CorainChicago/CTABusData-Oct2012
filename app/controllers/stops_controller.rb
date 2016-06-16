07d3d59de23b62eb2e51435e0af71806b388b42b
CorainChicago <chaysmagan@gmail.com>
2016-06-16 00:52:20 -0500

    cleaned up stop.rb, no code change

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,17 +1,6 @@
 class StopsController < ApplicationController
 
-  # def data_bus_stops
-  #   hash = BusRoute.bus_hash_by_stop_count
-  #   stop_array = BusRoute.order_by_stops(hash)
-  #   respond_to do |format|
-  #     format.json {
-  #       render :json => [stop_array, stop_array.map{|d| d[1] }.flatten]
-  #     }
-  #   end
-  # end
-
   def data_stop_location_and_bus_count
-    puts "hi"
     respond_to do |format|
       format.json {
         render :json => Stop.hash_by_location_and_bus_count.to_json
