88ab00895a3baea682c5b544aac7704b852ce7e7
CorainChicago <chaysmagan@gmail.com>
2016-06-20 21:21:33 -0500

    delete unnecessary files

--- spec/controllers/bus_routes_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe BusRoutesController, type: :controller do
-
-end
--- spec/controllers/stops_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe StopsController, type: :controller do
-
-end
